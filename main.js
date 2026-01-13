fd565d9564622a3f77e410657443b84bdddd6ffc
Kiril Bormin <borminnkiril@gmail.com>
2026-01-13 17:20:54 +0100

    feat (exercice) : L8

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            selectedVariant: 0,
             variants: [
+<<<<<<< Updated upstream
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+=======
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+>>>>>>> Stashed changes
             ]
         }
     },
@@ -17,6 +23,7 @@ const app = Vue.createApp({
         addToCart(){
             this.cart += 1
         },
+<<<<<<< Updated upstream
         updateImage(variantImage){
             this.image = variantImage
         },
@@ -25,6 +32,21 @@ const app = Vue.createApp({
                 this.cart -= 1
             }
             else{}
+=======
+        updateVariant(index){
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },  
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+>>>>>>> Stashed changes
         }
     }
 })
